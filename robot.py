10f7497e7e6207c2160d361eed72e156fe2d5a03
marcel <me@marcelcutts.com>
2016-02-28 22:57:26 +0000

    Added the robot element that holds and updates its perceived positional state

---
+++ robot.py
@@ -0,0 +1,53 @@
+class Robot(object):
+    """
+    Contains the spatial state of the robot as it bravely around mars,
+    as well as methods to update those states.
+    """
+    def __init__(self, initial_x, initial_y, initial_orientation, movement_instructions):
+        self.current_x = initial_x
+        self.current_y = initial_y
+        self.current_orientation = initial_orientation
+        self.movement_instructions = movement_instructions
+
+    def move_forward(self):
+        """
+        Moves our intrepid little rover forward 1 coordinate
+        :return: Updated x, y coordinates of rover
+        """
+        # In most languages, this would likely be a switch
+        # statement, but python has chosen not to include one
+        # due to its "one and only one way of doing something" philosophy
+        if self.current_orientation == 'N':
+            self.current_y += 1
+        if self.current_orientation == 'E':
+            self.current_x += 1
+        if self.current_orientation == 'S':
+            self.current_y -= 1
+        if self.current_orientation == 'W':
+            self.current_x -= 1
+        return self.current_x, self.current_y
+
+    def turn(self, direction):
+        """
+        Lets turn the rover! The rover can be on one of four compass
+        orientations, and turning left or right can be thought of as going
+        left or right on a clock. Therefore if you go left from E, you get N,
+        and if you 'overflow' on either side, you simply wrap round, e.g.
+        right from W would return to N
+        :param direction - The direction, 'L' or 'R', we'd like to turn
+        :returns: Updated orientation
+        """
+        orientations = ('N', 'E', 'S', 'W')
+        current_orientation_index = orientations.index(self.current_orientation)
+
+        if direction == 'L':
+            current_orientation_index -= 1
+            if current_orientation_index < 0:
+                current_orientation_index += 4
+        if direction == 'R':
+            current_orientation_index += 1
+            if current_orientation_index > 3:
+                current_orientation_index -= 4
+
+        self.current_orientation = orientations[current_orientation_index]
+        return self.current_orientation
---
+++ test/test_robot.py
@@ -0,0 +1,70 @@
+import unittest
+from robot import Robot
+
+
+class TestMoving(unittest.TestCase):
+    def test_move_forward_north(self):
+        north_bot = Robot(5, 5, 'N', 'F')
+        north_bot.move_forward()
+        self.assertEqual(north_bot.current_x, 5)
+        self.assertEqual(north_bot.current_y, 6)
+
+    def test_move_forward_east(self):
+        north_bot = Robot(11, 7, 'E', 'F')
+        north_bot.move_forward()
+        self.assertEqual(north_bot.current_x, 12)
+        self.assertEqual(north_bot.current_y, 7)
+
+    def test_move_forward_south(self):
+        north_bot = Robot(3, 4, 'S', 'F')
+        north_bot.move_forward()
+        self.assertEqual(north_bot.current_x, 3)
+        self.assertEqual(north_bot.current_y, 3)
+
+    def test_move_forward_west(self):
+        north_bot = Robot(1, 0, 'W', 'F')
+        north_bot.move_forward()
+        self.assertEqual(north_bot.current_x, 0)
+        self.assertEqual(north_bot.current_y, 0)
+
+
+class TestTurning(unittest.TestCase):
+    def test_turn_left_from_north(self):
+        north_bot = Robot(5, 5, 'N', 'L')
+        north_bot.turn('L')
+        self.assertEqual(north_bot.current_orientation, 'W')
+
+    def test_turn_right_from_north(self):
+        north_bot = Robot(5, 5, 'N', 'R')
+        north_bot.turn('R')
+        self.assertEqual(north_bot.current_orientation, 'E')
+
+    def test_turn_left_from_east(self):
+        north_bot = Robot(5, 5, 'E', 'L')
+        north_bot.turn('L')
+        self.assertEqual(north_bot.current_orientation, 'N')
+
+    def test_turn_right_from_east(self):
+        north_bot = Robot(5, 5, 'E', 'R')
+        north_bot.turn('R')
+        self.assertEqual(north_bot.current_orientation, 'S')
+
+    def test_turn_left_from_south(self):
+        north_bot = Robot(5, 5, 'S', 'L')
+        north_bot.turn('L')
+        self.assertEqual(north_bot.current_orientation, 'E')
+
+    def test_turn_right_from_south(self):
+        north_bot = Robot(5, 5, 'S', 'R')
+        north_bot.turn('R')
+        self.assertEqual(north_bot.current_orientation, 'W')
+
+    def test_turn_left_from_west(self):
+        north_bot = Robot(5, 5, 'W', 'L')
+        north_bot.turn('L')
+        self.assertEqual(north_bot.current_orientation, 'S')
+
+    def test_turn_right_from_west(self):
+        north_bot = Robot(5, 5, 'W', 'R')
+        north_bot.turn('R')
+        self.assertEqual(north_bot.current_orientation, 'N')
